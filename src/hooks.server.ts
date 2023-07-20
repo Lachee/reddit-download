807cb97f3b40440a2cb47392b315ed38fe461aad
Lachee <lachlan.h@hotmail.com>
2023-07-20 18:01:08 +1000

    added security to prevent CORS leak It was possible in previous versions  to use my site to strip cors form other people. I pay for this site; Only i can do this.... screw that!

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -1,17 +1,28 @@
 import type { Handle } from '@sveltejs/kit';
 
+export const AllowedRootDomains = [
+    'reddit.com',
+    'redd.it',
+    'redditstatic.com',
+    'redditmedia.com',
+];
+
+const rootDomain = (url : string) => (new URL(url)).hostname.split('.').reverse().splice(0,2).reverse().join('.')
+
 export const handle = (async ({ event, resolve }) => {
     const { url, fetch } = event;
     if (url.pathname.startsWith('/download')) {
-        const file = url.searchParams.get('get');
-        if (file != null) {
-            const response = await fetch(file);
-            const body = await response.body;
-            return new Response(body, { 
-                headers: {
-                    'content-type': response.headers.get('content-type') || 'image/gif',
-                }
-            });
+        const proxyUrl = url.searchParams.get('get');
+        if (proxyUrl != null) {
+            if (AllowedRootDomains.includes(rootDomain(proxyUrl))) {
+                const response = await fetch(proxyUrl);
+                const body = await response.body;
+                return new Response(body, { 
+                    headers: {
+                        'content-type': response.headers.get('content-type') || 'image/gif',
+                    }
+                });
+            }
         }
     }
 
