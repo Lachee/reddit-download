29198d93cf620c1ba31d195976987915b298133d
Lachee <lachlan.h@hotmail.com>
2023-09-07 17:00:22 +1000

    added a note

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -29,6 +29,7 @@ export const handle = (async ({ event, resolve }) => {
                     method: 'HEAD', 
                     redirect: 'follow' ,
                     headers: {
+                        // User-Agent because reddit will block CloudFlare's Worker user agent.
                         "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.97 Safari/537.36",
                     }
                 });
