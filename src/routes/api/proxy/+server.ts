44b6156c0b6930c55858387450f238802c747d59
Lachee <lachlan.h@hotmail.com>
2024-01-30 14:52:35 +1100

    added cloudflare compat b64 encoding

--- src/routes/api/proxy/+server.ts
+++ src/routes/api/proxy/+server.ts
@@ -8,6 +8,27 @@ import { getCache, normalize } from '$lib/cache';
 const STORE_TTL = 3600;
 const CACHE_TTL = STORE_TTL;
 
+function log(...args: any[]) {
+    console.log('[PROXY]', ...args);
+}
+function toB64(data: ArrayBuffer): string {
+    let binary = '';
+    const bytes = new Uint8Array(data);
+    const len = bytes.byteLength;
+    for (let i = 0; i < len; i++) {
+        binary += String.fromCharCode(bytes[i]);
+    }
+    return btoa(binary);
+}
+function fromB64(b64: string): ArrayBuffer {
+    const binaryString = atob(b64);
+    let bytes = new Uint8Array(binaryString.length);
+    for (let i = 0; i < binaryString.length; i++) {
+        bytes[i] = binaryString.charCodeAt(i);
+    }
+    return bytes.buffer;
+}
+
 export const GET: RequestHandler = async (request) => {
     const query = request.url.searchParams;
 
@@ -26,21 +47,25 @@ export const GET: RequestHandler = async (request) => {
     const fileExt = extname(fileName);
     const key = normalize(`proxy:${href}`);
 
-    let body : ArrayBuffer = new ArrayBuffer(0);
-    let contentType : string = '';
+    let body: ArrayBuffer | undefined;
+    let contentType: string | undefined;
 
     // Look for the cached content
     const cached = await getCache().get(key);
     if (cached != null && cached.startsWith('data:')) {
-        const [ type, encodedData ] = cached.split(';', 2);
+        /// Decode the data:mime;base64,content data uri
+        const [type, encodedData] = cached.split(';', 2);
         contentType = type.substring(5);
-
-        const [ encoding, data ] = encodedData.split(',', 2);
-        body = Buffer.from(data, encoding as BufferEncoding);
+        const [encoding, data] = encodedData.split(',', 2);
+        if (encoding === 'base64') {
+            body = fromB64(data);
+        }
     }
-    else 
-    {
+
+    // We failed to fetch something, so lets just pull it again
+    if (contentType === undefined || body === undefined) {
         // Fetch the content and best type
+        log('downloading', href.toString());
         const response = await fetch(href, { headers: { 'origin': 'reddit.com', 'User-Agent': UserAgent } });
         if (response.status != 200)
             return new Response(await response.body, { status: response.status });
@@ -48,8 +73,8 @@ export const GET: RequestHandler = async (request) => {
         body = await response.arrayBuffer();
         contentType = MIME[fileExt] || response.headers.get('content-type') || 'image/gif';
     }
-    
-    const serialized = `data:${contentType};base64,` + Buffer.from(body).toString('base64');
+
+    const serialized = `data:${contentType};base64,` + toB64(body);
     getCache().put(key, serialized, { expirationTtl: STORE_TTL });
 
     return new Response(body, {
